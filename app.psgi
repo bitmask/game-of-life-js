465e778b513efe94749c70ecda7535f74b3f24a0
Ingy dot Net <ingy@ingydotnet1.(none)>
2010-01-06 23:48:13 -0800

    Got basic game board drawn...

--- app.psgi
+++ app.psgi
@@ -1,13 +1,25 @@
 use strict;
 use warnings;
-
-open INDEX, "index.html";
-my $index = do { local $/; <INDEX> };
+use XXX;
 
 return sub {
+    my $env = shift;
+    my $path = $env->{PATH_INFO};
+
+    $path =~ s{^/}{};
+    $path ||= 'index.html';
+
+    my $content_type = 
+    $path =~ /\.css$/ ? 'text/css' :
+    $path =~ /\.js$/ ? 'application/javascript' :
+    'text/html';
+
+    open FILE, $path or die $!;
+    my $file = do { local $/; <FILE> };
+
     [
         200,
-        [ 'Content-Type' => 'text/html' ],
-        [ $index ]
+        [ 'Content-Type' => $content_type ],
+        [ $file ]
     ];
 }
--- js/game-of-life.js
+++ js/game-of-life.js
@@ -1,3 +1,11 @@
+String.prototype.times = function(n) {
+    var a = [];
+    for (var i = 0; i < n; i++) {
+        a.push(this);
+    }
+    return a.join('');
+};
+
 (Life = function() {}).prototype = {
     setup: function() {
         // Generate a game board - an almost full screen square
@@ -7,7 +15,17 @@
     },
 
     generate_game_board: function() {
-        console.log('generate_game_board');
+        var width = $(window).width();
+        var height = $(window).height();
+        var cell_width = 10;
+        var padding = 10;
+        var side = parseInt((Math.min(width, height) - 2 * padding) / cell_width) * cell_width;
+        var cells = side / cell_width;
+        var row = '<tr>' + '<td></td>'.times(cells) + '</tr>';
+        var table = '<table>' + row.times(cells) + '</table>';
+        $('body').append(table);
+        $('td').width(cell_width).height(cell_width);
+        $('body').append('<input type="submit" value="Start" />');
     },
 
     run: function() {
